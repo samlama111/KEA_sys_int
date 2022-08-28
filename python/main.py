b3b9cd93ddcfdaab39e219d60bb2e2bb804359bf
samlama111 <sam.horacek@gmail.com>
2022-08-28 18:15:26 +0200

    Updated missing python formats

--- python/main.py
+++ python/main.py
@@ -1,12 +1,36 @@
 import pandas as pd
+import yaml 
 
-csv_person = pd.read_csv(r'file_types\person.csv')
+file_path_template = r'file_types\person.'
 
-json_person = csv_person.to_json()
+# Following formats can be loaded
+# and worked with, either in pandas DataFrame format
+# or in JSON
 
+# CSV
+csv_file = pd.read_csv(file_path_template+'csv')
+csv_person = csv_file.to_csv()
+
+# Then they can be transformed easily 
+# into other formats, as with this CSV example
+csv_json_person = csv_file.to_json()
 # issues with XML conversion, so HTML instead
-xml_person = csv_person.to_html()
+csv_xml_person = csv_file.to_html()
+csv_txt_person = csv_file.to_string()
 
-txt_person = csv_person.to_string()
 
-print(txt_person)
+# JSON
+json_file = pd.read_json(file_path_template+'json')
+json_person = json_file.to_json()
+
+# XML
+xml_file = pd.read_xml(file_path_template+'xml')
+xml_person = xml_file.to_xml()
+
+# TXT
+with open(file_path_template+'txt') as f:
+    lines = f.readlines()
+
+# YAML
+with open(file_path_template+'yml', 'r') as file:
+    yaml_person = yaml.safe_load(file)
