2af8d96fbc84cc8327d4579032e461401dc0bffa
samlama111 <sam.horacek@gmail.com>
2022-08-29 20:48:38 +0200

    JS - added comments and logs

--- 1-file_formats/javascript/index.js
+++ 1-file_formats/javascript/index.js
@@ -4,6 +4,9 @@ import fs from 'fs/promises'
 const path_to_file_template = '../file_types/person.'
 
 const txtFile = await readTxtFile(path_to_file_template+'txt')
+
+// data parsed from txt file to be used as JSON
+// could be done for other formats as well
 const parsedTxtFileFromJson = txtToJson(txtFile)
 
 const csv_data = await fs.readFile(path_to_file_template+'csv', {encoding: 'utf8'});
@@ -13,3 +16,13 @@ const json_data = await fs.readFile(path_to_file_template+'json', {encoding: 'ut
 const xml_data = await fs.readFile(path_to_file_template+'xml', {encoding: 'utf8'});
 
 const yml_data = await fs.readFile(path_to_file_template+'yml', {encoding: 'utf8'});
+
+console.log(txtFile)
+console.log('-----------')
+console.log(csv_data)
+console.log('-----------')
+console.log(json_data)
+console.log('-----------')
+console.log(xml_data)
+console.log('-----------')
+console.log(yml_data)
