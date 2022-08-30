715e9f31480c08fb14f0efd65e4519c1c6e90b2b
samlama111 <sam.horacek@gmail.com>
2022-08-30 16:33:06 +0200

    Fetching & parsing from Python to JS implemented

--- 2-date-servers/javascript/app.js
+++ 2-date-servers/javascript/app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import fetch from 'node-fetch'
 
 const app = express()
 
@@ -10,6 +11,12 @@ app.get("/timestamp", (req, res) => {
     res.send(new Date())
 })
 
+app.get("/get/timestamp", async (req, res) => {
+    const response = await fetch("http://127.0.0.1:8000/timestamp")
+    const dateObject = await response.json()
+    const newDateObject = new Date(dateObject.Date)
+    res.send(newDateObject)
+})
 app.listen(3000, () => {
 	console.log(`Date app listening on port 3000`)
 })
