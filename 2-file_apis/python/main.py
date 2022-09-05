909e958fe7f2813993db108ccab3b4e471e0798b
samlama111 <sam.horacek@gmail.com>
2022-09-05 19:29:52 +0200

    Implementation of Python file format parser - handling csv, json and xml

---
+++ 2-file_apis/python/main.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from fastapi import FastAPI
+
+app = FastAPI()
+
+file_path_template =r'..\..\1-file_formats\file_types\person.'
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/csv")
+def read_csv():
+    csv_file = pd.read_csv(file_path_template+'csv')
+    csv_json_person = csv_file.to_json()
+    return csv_json_person
+
+@app.get("/json")
+def read_json():
+    json_file = pd.read_json(file_path_template+'json')
+    json_person = json_file.to_json()
+    return json_person
+
+@app.get("/xml")
+def read_xml():
+    xml_file = pd.read_xml(file_path_template+'xml')
+    xml_person = xml_file.to_json()
+    return xml_person
