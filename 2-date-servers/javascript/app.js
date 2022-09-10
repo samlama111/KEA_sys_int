497af1132f164986e07769e8cbba25062dc3f69b
samlama111 <sam.horacek@gmail.com>
2022-09-10 13:49:27 +0200

    Date auth. Swagger set-up

--- 2-date-servers/javascript/app.js
+++ 2-date-servers/javascript/app.js
@@ -1,16 +1,47 @@
 import express from 'express'
 import fetch from 'node-fetch'
+import swaggerJSDoc from 'swagger-jsdoc'  
+import swaggerUI from 'swagger-ui-express'
 
 const app = express()
 
-app.get("/", (req, res) => {
-    console.log('kkt')
-})
+//Swagger Configuration  
+const swaggerOptions = {  
+    swaggerDefinition: {  
+        info: {  
+            title:'Date authority API',  
+            version:'1.0.0'  
+        }  
+    },  
+    apis:['app.js'],  
+}  
+const swaggerDocs = swaggerJSDoc(swaggerOptions);  
+app.use('/docs',swaggerUI.serve,swaggerUI.setup(swaggerDocs));  
 
+/** 
+ * @swagger 
+ * /Timestamp: 
+ *   get: 
+ *     description: Get Current Timestamp
+ *     responses:  
+ *       200: 
+ *         description: Success  
+ *   
+ */  
 app.get("/timestamp", (req, res) => {
     res.send(new Date())
 })
 
+/** 
+ * @swagger 
+ * /Get/Timestamp: 
+ *   get: 
+ *     description: Get Timestamp From Authority
+ *     responses:  
+ *       200: 
+ *         description: Success  
+ *   
+ */ 
 app.get("/get/timestamp", async (req, res) => {
     try {
         const response = await fetch("http://127.0.0.1:8000/timestamp")
