63aa3a906964fd56aa1d9a37deb9fa1da513a403
samlama111 <sam.horacek@gmail.com>
2022-09-10 13:48:55 +0200

    Added error-handling to date auth. fetching

--- 2-date-servers/javascript/app.js
+++ 2-date-servers/javascript/app.js
@@ -12,11 +12,16 @@ app.get("/timestamp", (req, res) => {
 })
 
 app.get("/get/timestamp", async (req, res) => {
-    const response = await fetch("http://127.0.0.1:8000/timestamp")
-    const dateObject = await response.json()
-    const newDateObject = new Date(dateObject.Date)
-    res.send(newDateObject)
+    try {
+        const response = await fetch("http://127.0.0.1:8000/timestamp")
+        const dateObject = await response.json()
+        const newDateObject = new Date(dateObject.Date)
+        res.send(newDateObject)
+    } catch (err) {
+        console.log('Connection to date authority failed')
+    }
 })
+
 app.listen(3000, () => {
 	console.log(`Date app listening on port 3000`)
 })
